2e14bf648bc0e0340a241257f6526b733720643b
George Davidson <gd240673@gmail.com>
2023-01-02 19:19:36 -0500

    Updated README.me

---
+++ README.md
@@ -0,0 +1,7 @@
+# PyQt GUI Test
+
+Playing around with the PyQt library.
+
+This is a basic PyQt window.
+
+![window.png](README.assets/window.png)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PyQt6.QtGui import QIcon
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(0, 0, 800, 500)
+        self.setGeometry(20, 50, 800, 500)
         self.setWindowTitle("Python")
         self.setWindowIcon(QIcon("icon/icon.png"))
         self.setMinimumSize(500, 300)
