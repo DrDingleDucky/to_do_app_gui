94406dd3575aa4770b8137952649edda74f937d1
George Davidson <gd240673@gmail.com>
2023-01-04 12:08:55 -0500

    To Do App
    Made a to do app

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QGridLayout
 from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QLabel,
+                             QLineEdit, QPushButton, QWidget)
+
+
+class Box:
+    def __init__(self, layout, row):
+        self.layout = layout
+        self.row = row
+
+        self.checkbox = QCheckBox()
+        self.layout.addWidget(self.checkbox, self.row, 0)
+
+        self.input = QLineEdit()
+        self.layout.addWidget(self.input, self.row, 1)
+
+        self.button = QPushButton("X")
+        self.button.clicked.connect(lambda: self.deleat())
+        self.layout.addWidget(self.button, self.row, 2)
+
+    def deleat(self):
+        self.checkbox.hide()
+        self.input.hide()
+        self.button.hide()
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        # self.setGeometry(20, 50, 800, 500)  # Window Size
-        self.setWindowTitle("Python")  # Window Title
+
+        self.row_index = 4  # Keep a record of the row.
+
+        self.setWindowTitle("To Do")  # Window Title
         self.setWindowIcon(QIcon("icon/icon.png"))  # Window Icon
-        # self.setMinimumSize(500, 300)  # The window's smallest possible size.
+        self.setMinimumSize(450, 250)  # The window's smallest possible size.
 
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
-        self.lable_1 = QLabel("Username")
-        self.layout.addWidget(self.lable_1, 0, 0)
+        Box(self.layout, 0)
+        Box(self.layout, 1)
+        Box(self.layout, 2)
+        Box(self.layout, 3)
+        Box(self.layout, 4)
 
-        self.lable_2 = QLabel("Email")
-        self.layout.addWidget(self.lable_2, 1, 0)
+        self.add_button = QPushButton("Add")
+        self.add_button.clicked.connect(self.add)
+        self.layout.addWidget(self.add_button, 6, 1)
 
-        self.input_1 = QLineEdit()
-        self.layout.addWidget(self.input_1, 0, 1)
+    def add(self):
+        self.row_index += 1
 
-        self.input_2 = QLineEdit()
-        self.layout.addWidget(self.input_2, 1, 1)
+        Box(self.layout, self.row_index)
+        self.layout.addWidget(self.add_button, self.row_index + 1, 1)
 
-        self.button = QPushButton("Sign In")
-        self.button.clicked.connect(self.display)
-        self.layout.addWidget(self.button, 2, 1)
-
-    def display(self):
-        print(self.input_2.text())
-        print(self.input_2.text())
+    def deleat(self):
+        print("deleat")
 
 
 def main():
